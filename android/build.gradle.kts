702c78f1da380ff3bc0dbc2107686171f70e53cf
Patipat Chewprecha <patipat.che@mahidol.edu>
2025-06-17 15:10:53 +0700

    Fix Java version warnings by updating to Java 11
    - Updated android/build.gradle.kts to configure Java 11 for all subprojects
    - Updated android/gradle.properties to remove invalid java.home setting
    - This resolves obsolete Java 8 warnings during compilation

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,6 +5,20 @@ allprojects {
     }
 }
 
+// Configure Java version for all subprojects
+subprojects {
+    afterEvaluate {
+        if (project.hasProperty("android")) {
+            project.extensions.configure<com.android.build.gradle.BaseExtension> {
+                compileOptions {
+                    sourceCompatibility = JavaVersion.VERSION_11
+                    targetCompatibility = JavaVersion.VERSION_11
+                }
+            }
+        }
+    }
+}
+
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
