1b2ecdb979a982d29fcd207cfa34fda15c15df5c
Patipat Chewprecha <patipat.che@mahidol.edu>
2025-06-17 13:21:15 +0700

    Remove macOS and Windows runner files and configurations
    - Deleted macOS runner files including DebugProfile.entitlements, Info.plist, MainFlutterWindow.swift, and Release.entitlements.
    - Removed macOS runner tests from RunnerTests.swift.
    - Deleted Windows-specific files including CMakeLists.txt, generated_plugin_registrant files, and various runner source files.
    - Cleaned up Windows runner resources and utility files.
    - Removed associated icon and manifest files for Windows runner.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.pdf_flipbook"
+    namespace = "th.ac.mahidol.int.ideaspace.pdf-flipbook"
     compileSdk = 35
     ndkVersion = "27.0.12077973"
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.pdf_flipbook"
+        applicationId = "th.ac.mahidol.int.ideaspace.pdf-flipbook"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 21
